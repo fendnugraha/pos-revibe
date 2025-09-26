46d42a35777efa30f3548ad49f5dcf1cf196383c
Fend Nugraha <fendnugraha@Fends-Book.local>
2025-09-26 17:38:55 +0700

    add warehouse stock

--- src/app/(app)/inventory/components/CreateStockAdjustment.js
+++ src/app/(app)/inventory/components/CreateStockAdjustment.js
@@ -144,7 +144,7 @@ const CreateStockAdjustment = ({ isModalOpen, product, warehouse, warehouses, no
                         }
                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                     />
-                    <label htmlFor="is_service" className="ml-2 text-sm font-medium text-gray-900">
+                    <label htmlFor="is_initial" className="ml-2 text-sm font-medium text-gray-900">
                         Sesuaikan sebagai stok awal persediaan
                     </label>
                 </div>
